158139225c91dcd38c04415267cfb7871cb28e49
BCCasino <BCCasino@focushere.ca>
2011-06-04 17:29:41 -0400

    Test with rpc move and multi accounts, allows 8 points of precision.

--- pages/Main.php
+++ pages/Main.php
@@ -1,4 +1,20 @@
 <?php
 	$bitcoin = new jsonRPCClient('http://BCCasino:Qwerty123456@127.0.0.1:8332/');
-	$this->bitcoin = $bitcoin;
+	$tis->bitcoin = $bitcoin;
+//	print_r($bitcoin->sendfrom("Test","1JHQCpyRHc4vsNHsDYKS41pFTUAV299HgT",0.02));
+//	$bitcoin->sendtoaddress("1JHQCpyRHc4vsNHsDYKS41pFTUAV299HgT",0.02);
+	
+	print_r($bitcoin->getaddressesbyaccount("Test"));
+	echo "<br/>";
+	print_r($bitcoin->getbalance("Test",0));
+	echo "<br/>";
+	print_r($bitcoin->getbalance("",0));
+	echo "<br/>";
+	print_r($bitcoin->move("Test","",(float)0.00000001));
+	echo "<br/>";
+	print_r($bitcoin->getbalance("Test",0));
+	echo "<br/>";
+	print_r($bitcoin->getbalance("",0));
+	echo "<br/>";
+	print_r($bitcoin->listtransactions("Test"));
 ?>
