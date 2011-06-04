0088d177bfa9d1b2376cd1873ef911dd43d5a148
BCCasino <BCCasino@focushere.ca>
2011-06-04 08:39:01 -0400

    playing with accounts, nailed it.

--- index.php
+++ index.php
@@ -7,7 +7,10 @@ function JSONtoAmount($value) {
   $bitcoin = new jsonRPCClient('http://BCCasino:Qwerty123456@127.0.0.1:8332/');
 
   echo "<pre>\n";
-  print_r($bitcoin->help()); echo "\n";
-  echo "Received: ".$bitcoin->getreceivedbylabel("Your Address")."\n";
+  print_r($bitcoin->getaccountaddress("test"));
+  echo "\n";
+  print_r($bitcoin->getbalance("test",3));
+  echo "\n";
+  echo "Received: ".$bitcoin->getreceivedbyaddress("18BMfDnLKrFP4xPAAPU7cF87fzQ4oEmnxE")."\n";
   echo "</pre>"
 ?>
