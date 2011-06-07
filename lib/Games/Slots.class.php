7251dfc19c8becb57fe031b3e48f70c1d98e51df
BCCasino <BCCasino@focushere.ca>
2011-06-07 13:56:58 -0400

    Added paytable for a real slot machine, will base our pays on this, using expected return theory

--- lib/Games/Slots.class.php
+++ lib/Games/Slots.class.php
@@ -9,19 +9,55 @@ class SlotMachine
 	
 	private $symbols = array("TripleBar" => 
 						array ("img" => "img/slots/tbar.png",
-							   "probability" => 0.01),
+							   "probability" => 0.01,
+							   "class" => array("")),
 					 "DoubleBar" => 
 						array ("img" => "img/slots/dbar.png",
-							   "probability" => 0.02),
+							   "probability" => 0.02,
+							   "class" => array("")),
 					 "Bar" 		 => 
 						array ("img" => "img/slots/bar.png",
-							   "probability" => 0.05),
+							   "probability" => 0.05,
+							   "class" => array("")),
 					 "Cherry"	 => 
 						array ("img" => "img/slots/cherry.png",
-							   "probability" => 0.1),
+							   "probability" => 0.1,
+							   "class" => array("")),
 					 "Blank" 	 =>
 						array ("img" => "img/slots/blank.png",
-							   "probability" => 0.83));
+							   "probability" => 0.83,
+							   "class" => array("")));
+	
+	private $payTable = array(array ("percoin" => 1,"maxcoinbonus" => 0,"probability" = > 0.125, 
+								"line" => array("blank","blank","blank")),
+							  array ("percoin" => 2,"maxcoinbonus" => 0,"probability" = > 0.002884, 
+								"line" => array("blue","blue","blue")),
+							  array ("percoin" => 2,"maxcoinbonus" => 0,"probability" = > 0.003952, 
+								"line" => array("white","white","white")),
+							  array ("percoin" => 2,"maxcoinbonus" => 0,"probability" = > 0.001278, 
+								"line" => array("red","red","red")),
+							  array ("percoin" => 5,"maxcoinbonus" => 0,"probability" = > 0.030430, 
+								"line" => array("bar","bar","bar")),
+							  array ("percoin" => 10,"maxcoinbonus" => 0,"probability" = > 0.001648, 
+								"line" => array("1bar","1bar","1bar")),
+							  array ("percoin" => 20,"maxcoinbonus" => 0,"probability" = > 0.000431, 
+								"line" => array("red","white","blue")),
+							  array ("percoin" => 25,"maxcoinbonus" => 0,"probability" = > 0.001442, 
+								"line" => array("2bar","2bar","2bar")),
+							  array ("percoin" => 40,"maxcoinbonus" => 0,"probability" = > 0.000801, 
+								"line" => array("3bar","3bar","3bar")),
+							  array ("percoin" => 50,"maxcoinbonus" => 0,"probability" = > 0.000687, 
+								"line" => array("1bar","2bar","3bar")),
+							  array ("percoin" => 80,"maxcoinbonus" => 0,"probability" = > 0.004574, 
+								"line" => array("seven","seven","seven")),
+							  array ("percoin" => 150,"maxcoinbonus" => 0,"probability" = > 0.000160, 
+								"line" => array("blue7","blue7","blue7")),
+							  array ("percoin" => 200,"maxcoinbonus" => 0,"probability" = > 0.000160, 
+								"line" => array("white7","white7","white7")),
+							  array ("percoin" => 1200,"maxcoinbonus" => 200,"probability" = > 0.000011, 
+								"line" => array("red7","red7","red7")),
+							  array ("percoin" => 2400,"maxcoinbonus" => 400,"probability" = > 0.000004, 
+								"line" => array("red7","white7","blue7")))
 	
 	private $reelLayout = array( array ("Blank",
 								"Cherry",
