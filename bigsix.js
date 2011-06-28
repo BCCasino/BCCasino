cd42db88c51b0edfec69c9ca22d9c6f988fdb1f9
BCCasino <BCCasino@focushere.ca>
2011-06-28 11:57:30 -0400

    placebet function, will need additional logic to handle a bet amount change/removal
    Signed-off-by: BCCasino <BCCasino@focushere.ca>

--- bigsix.js
+++ bigsix.js
@@ -1,3 +1,8 @@
+var Wheel = [ 40,2,1,2,5,2,1,2,1,10,2,5,1,2,1,20,1,5,1,2,1,5,1,2,1,40,1,5,1,2,1,10,1,2,1,5,1,2,1,20,1,2,1,2,1,5,1,10,1,2 ];
+var Bets = [1,2,5,10,20,40];
+
+var Bets = [];
+
 exports.Spin =function() {
 		return Math.floor(Math.random()*52)+1;
 	}; 
@@ -11,6 +16,8 @@ var chars = "0123456789ABCDEF";
 }
 return [Hash.sha512(spin+salt),salt];
 };
+
+
 var bitcoin = require('bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456');
 require('joose')
@@ -20,7 +27,19 @@ require('joose')
 exports.MaxBet = function(callback) {
 	client.getBalance("",function(err,bal) {
 		require('util').debug("Balance "+bal);
-		var max = (bal/3)/45;
+		var max = (bal/3)/40;
 		callback(max);
 });
 };
+
+exports.PlaceBet = function(secret,bet,amount,callback) {
+	exports.MaxBet(function(max) {
+		if(amount < max)
+		{
+			Bets.push(secret,[bet,amount]);
+			callback(true);
+		}
+		else
+			callback(false);
+	});
+}
