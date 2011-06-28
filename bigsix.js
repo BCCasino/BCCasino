080285b3061b3386671184d165c0c4be0dc4f85e
BCCasino <BCCasino@focushere.ca>
2011-06-28 08:43:33 -0400

    added hashing

--- bigsix.js
+++ bigsix.js
@@ -1,8 +1,21 @@
 exports.Spin =function() {
 		return Math.floor(Math.random()*52)+1;
 	};
+exports.SpinHash = function(spin) { 
+var salt = "";
+var chars = "0123456789ABCDEF";
+	for(var x=0;x<40;x++)
+{
+	var n = Math.floor(Math.random()*chars.length);
+	salt+=chars.substring(n,n+1);
+}
+return [Hash.sha512(spin+salt),salt];
+};
 var bitcoin = require('bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456');
+require('joose')
+    require('joosex-namespace-depended')
+    require('hash')
 
 exports.MaxBet = function(callback) {
 	client.getBalance("",function(err,bal) {
