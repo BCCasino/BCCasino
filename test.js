202d5e012104dd64400b0fd89fb7cf0fe23010bf
BCCasino <BCCasino@focushere.ca>
2011-06-29 14:18:46 -0400

    working, now up the test size
    Signed-off-by: BCCasino <BCCasino@focushere.ca>

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ var bitcoin = require('bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456'); 
 var bs = new BigSixWheel();
 var results = new Object();
-for(var x=0;x<500;x++){
+for(var x=0;x<50000;x++){
 	var spin = bs.Spin()[0];
 	if(results[bs.getBets()[bs.getWheel()[spin]]] ===undefined)
 		results[bs.getBets()[bs.getWheel()[spin]]]=0;
