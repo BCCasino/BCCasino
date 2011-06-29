f5240018990bdba6a2399b91050485463deaf12d
BCCasino <BCCasino@focushere.ca>
2011-06-29 14:47:45 -0400

    asdf
    Signed-off-by: BCCasino <BCCasino@focushere.ca>

--- test.js
+++ test.js
@@ -3,12 +3,12 @@ var bitcoin = require('bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456'); 
 var bs = new BigSixWheel();
 var results = new Object();
-for(var x=0;x<50000;x++){
+for(var x=0;x<500000;x++){
 	var spin = bs.Spin()[0];
 	if(results[bs.getWheel()[spin]] ===undefined)
 		results[bs.getWheel()[spin]]=0;
 	results[bs.getWheel()[spin]]++;
-	console.log("Spin "+x+": "+spin);	
+	//console.log("Spin "+x+": "+spin);	
 }
 console.log("BETS: "+bs.getBets().length);
 for(var word in results)
