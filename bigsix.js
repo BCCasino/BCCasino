963ad9bd216a3e314609b6b70fb2c9d5ea4b8edb
cold <ahuntdesign@gmail.com>
2011-06-28 20:02:08 -0400

    Revert 36fa40d73c355977370ef58a2a2491de20d3a7fb^..HEAD

--- bigsix.js
+++ bigsix.js
@@ -1,43 +1,40 @@
+var Wheel = [ "flag","1","2","1","2","5","2","1","2","1","10","2","5","1","2","1","20","1","5","1","2","1","5","1","2","1","joker",
+"1","5","1","2","1","10","1","2","1","5","1","2","1","20","1","2","1","2","1","5","1","10","1","2","1" ];
+var Bets = {"1":1,"2":2,"5":5,"10":10,"20":20,"flag":40,"joker":40};
+
+var PlayedBets = [];
+var spun = -1;
+
+exports.Spin =function() {
+		return Math.floor(Math.random()*52)+1;
+	}; 
+exports.SpinHash = function(spin) { 
+var salt = "";
+var chars = "0123456789ABCDEF";
+	for(var x=0;x<40;x++)
+{
+	var n = Math.floor(Math.random()*chars.length);
+	salt+=chars.substring(n,n+1);
+}
+return [Hash.sha512(spin+salt),salt];
+};
+
+
 var bitcoin = require('bitcoin');
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456');
-require('joose');
-require('joosex-namespace-depended');
-require('hash');
+require('joose')
+    require('joosex-namespace-depended')
+    require('hash')
 
-exports.BigSix = {
-
-
-Wheel: [ "flag","1","2","1","2","5","2","1","2","1","10","2","5","1","2","1","20","1","5","1","2","1","5","1","2","1","joker",
-"1","5","1","2","1","10","1","2","1","5","1","2","1","20","1","2","1","2","1","5","1","10","1","2","1" ],
-Bets: {"1":1,"2":2,"5":5,"10":10,"20":20,"flag":40,"joker":40},
-PlayedBets: [],
-spun: [],
-
-Spin: function() {
-		var spin = Math.floor(Math.random()*52)+1;
-		spun=[spin,exports.SpinHash(spin)];
-		return spun;
-	},
-SpinHash: function(spin) { 
-	var salt = "";
-	var chars = "0123456789ABCDEF";
-	for(var x=0;x<40;x++)
-	{
-		var n = Math.floor(Math.random()*chars.length);
-		salt+=chars.substring(n,n+1);
-	}
-	return [Hash.sha512(spin+salt),salt];
-},
-
-MaxBet: function(callback) {
+exports.MaxBet = function(callback) {
 	client.getBalance("",function(err,bal) {
 		require('util').debug("Balance "+bal);
 		var max = (bal/3)/40;
 		callback(Math.min(max,1));
-	});
-},
+});
+};
 
-PlaceBet: function(room,secret,bet,amount,callback) {
+exports.PlaceBet = function(secret,bet,amount,callback) {
 	exports.MaxBet(function(max) {
 		if(amount < max)
 		{
@@ -54,9 +51,9 @@ PlaceBet: function(room,secret,bet,amount,callback) {
 		else
 			callback(false);
 	});
-},
+}
 
-PayBets: function() {
+exports.PayBets = function() {
 	var winningBet = Wheel[spun];
 	for(var x =0;x<PlayedBets.length;x++)
 	{
@@ -73,4 +70,3 @@ PayBets: function() {
 		}
 	}
 }
-};
