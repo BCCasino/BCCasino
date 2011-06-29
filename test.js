3a616ec55861a7335aab03eb9797cab858694f51
BCCasino <BCCasino@focushere.ca>
2011-06-29 12:21:40 -0400

    Stupid non-blank commit message Signed-off-by: BCCasino <BCCasino@focushere.ca>

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ require('./bigsix.js');
 var bitcoin = require('bitcoin'); 
 var client = new bitcoin.Client('localhost', 8332, 'bitcoin', 'Qwerty123456'); 
 var bs = new BigSixWheel();
-
+Console.log("First Spin: "+bs.Spin());
 var express = require('express'); var jade = require('jade'); app = express.createServer(); app.get('/', 
 function(req, res) {
 	var local_var = "Balance: ";
