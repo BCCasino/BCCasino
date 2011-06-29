511aa1542a682c815536d63946887d21853c189f
BCCasino <BCCasino@focushere.ca>
2011-06-29 13:28:51 -0400

    test
    Signed-off-by: BCCasino <BCCasino@focushere.ca>

--- test.js
+++ test.js
@@ -10,7 +10,7 @@ for(var x=0;x<5000;x++){
 	results[bs.getBets()[bs.getWheel()[spin]]]++;
 	console.log("Spin "+x+": "+spin);	
 }
-
+console.log("BETS: "+bs.getBets());
 for(var y=0;y<bs.getBets().length;y++)
 {
 	console.log("BET: "+bs.getBets()[y] +" Count: "+results[bs.getBets()[y]]);
