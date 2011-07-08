59b74a3b06ba3b18ace73db21bfbbeb6a293e92c
cold <ahuntdesign@gmail.com>
2011-07-07 21:10:41 -0400

    Server.js updated to test2.js functionality.

--- server.js
+++ server.js
@@ -1,12 +1,31 @@
-//setup Dependencies
-require(__dirname + "/lib/setup").ext( __dirname + "/lib").ext( __dirname + "/lib/express/support");
-require(__dirname + "/lib/bigsix.js");
-var connect = require('connect')
-    , express = require('express')
-    , sys = require('sys')
-    , io = require('Socket.IO-node')
-    , port = (process.env.PORT || 8081)
-	, bitcoin = require('bitcoin');
+var https = require('https');
+var fs = require('fs');
+/**
+ * Bootstrap app.
+ */
+var sys = require('sys')
+require.paths.unshift(__dirname + '/../../lib/');
+
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , sio = require('socket.io'),
+	connect = require('connect'),
+	Room = require("./lib/room.js");
+	
+var port = 8081;
+
+/**
+ * App.
+ */
+//var privateKey = fs.readFileSync('../www.thebitcoinwheel.com.key').toString();
+//var certificate = fs.readFileSync('../www.thebitcoinwheel.com.crt').toString();
+//var ca = fs.readFileSync('../intermediate.crt').toString();
+
+//var server = express.createServer({key:privateKey,cert:certificate,ca:ca });
+var server = express.createServer();
 
 //Setup Express
 var server = express.createServer();
@@ -22,55 +41,13 @@ server.configure('production', function(){
 	server.use(express.errorHandler());
 });
 
-//setup the errors
-server.error(function(err, req, res, next){
-    if (err instanceof NotFound) {
-        res.render('404.ejs', { locals: { 
-                  footer: ''
-                 ,title : '404 Not Found'
-                },status: 404 });
-    } else {
-        res.render('500.ejs', { locals: { 
-                  footer: ''
-                 ,title : 'The Server Encountered an Error'
-                 ,error: err 
-                },status: 500 });
-    }
-});
-server.listen( port);
-
-//Setup Socket.IO
-var io = io.listen(server);
-io.on('connection', function(client){
-	console.log('Client Connected');
-	
-	client.on('message', function(message){
-		client.broadcast(message);
-		client.send(message);
-	});
-	client.on('disconnect', function(){
-		console.log('Client Disconnected.');
-	});
-});
-
-//Setup Bitcoin stuff & wheel stuff
-var bs = new BigSixWheel();
-
 ///////////////////////////////////////////
 //              Routes                   //
 ///////////////////////////////////////////
 
 /////// ADD ALL YOUR ROUTES HERE  /////////
 
-server.get('/', function(req, res) {
-/*	bs.MaxBet(function(mb) {
-		res.render('index.ejs', {
-			locals : { 
-				footer: ''
-				,title : 'Home - Max Bet: ' + mb
-			}
-		});
-	});*/
+server.get('*', function(req, res) {
 	res.render('index.ejs', {
 		locals : { 
 			footer: ''
@@ -80,21 +57,44 @@ server.get('/', function(req, res) {
 });
 
 
-//A Route for Creating a 500 Error (Useful to keep around)
-server.get('/500', function(req, res){
-    throw new Error('This is a 500 Error');
+//SOCKET IO
+//Setup Socket.IO
+/*var io = sio.listen(server,{
+	key: privateKey,
+	cert: certificate,
+	ca: ca,
+	secure: true
+});*/
+var io = sio.listen(server);
+var rooms = [];
+var rmchannel = 0;
+console.log("test");
+io.sockets.on('connection', function(socket) {
+	console.log('Got a connection');
+	var fnd = null;
+	for(var r in rooms)
+	{
+		var n = rooms[r].numberOfPlayers();		
+		if(n<5)
+		{
+			fnd = rooms[r];
+			console.log("Found a non-full room: "+fnd.getChannel());
+			break;
+		}
+	}
+	if(fnd!=null)
+	{
+		socket.emit("JoinRoom",fnd.getChannel());
+	}
+	else
+	{
+		fnd = new Room(io, '/' + rmchannel++);
+		console.log("Room created: " + fnd.getChannel());
+		socket.emit("JoinRoom", fnd.getChannel());
+		rooms.push(fnd);
+	}
 });
 
-//The 404 Route (ALWAYS Keep this as the last route)
-server.get('/*', function(req, res){
-    throw new NotFound;
-});
-
-function NotFound(msg){
-    this.name = 'NotFound';
-    Error.call(this, msg);
-    Error.captureStackTrace(this, arguments.callee);
-}
-
+server.listen(port);
 
 console.log('Listening on http://0.0.0.0:' + port );
--- static/js/script.js
+++ static/js/script.js
@@ -16,68 +16,49 @@ var Wheel = {
 	}
 };
 
+function message (from, msg) {
+	alert(from + ":\n" + msg);
+}
+
 $(document).ready(function() {
 	// socket.io specific code
-	var socket = new io.Socket(null, { secure: true });
+	var socket = io.connect(null,{rememberTransport: false});
 	var socket2;
 	socket.on('connect', function () {
-	  $('#chat').addClass('connected');
-	  message('System','connected');
+		message('System','connected');
 	});
 
 	socket.on('JoinRoom',function(msg){
-	  message('System','Join Room: '+msg);
-	  socket2 = io.connect(msg);
-	  socket2.on('connect', function() {
-	    socket2.emit('join',{/*secret*/});
-	    message('System','Joined room: '+msg);
-	     socket2.on('spin',function(msg){
-	       message('System','Spin: '+msg.spin+', salt: '+msg.salt);
-	     });
-	     socket2.on('newHash',function(msg){
-	       message('System','Hash: '+ msg.hash);
-	    });
-	     socket2.on('timeLeft',function(msg){
-	      message('System','Time Left: '+msg);
-	    });
-	     socket2.on('DepositAddress',function(msg) {
-	      message('System','Deposit Address: ' +msg);
-	   });
-	     socket2.on('Balance',function(msg) {
-	      message('System','Balance: '+msg);
-	    });
-	     socket2.on('Secret',function(msg){
-	      message('System','Secret: '+msg);
-	    });
-	    setInterval(function() { 
-	       socket2.emit('timeLeft');
-	      },1000);
-	    })
+		message('System','Join Room: '+msg);
+		socket2 = io.connect(msg);
+		socket2.on('connect', function() {
+		 socket2.emit('join',{/*secret*/});
+		 message('System','Joined room: '+msg);
+		  socket2.on('spin',function(msg){
+		    message('System','Spin: '+msg.spin+', salt: '+msg.salt);
+		  });
+		  socket2.on('newHash',function(msg){
+		    message('System','Hash: '+ msg.hash);
+		 });
+		  socket2.on('timeLeft',function(msg){
+		   message('System','Time Left: '+msg);
+		 });
+		  socket2.on('DepositAddress',function(msg) {
+		   message('System','Deposit Address: ' +msg);
+		});
+		  socket2.on('Balance',function(msg) {
+		   message('System','Balance: '+msg);
+		 });
+		  socket2.on('Secret',function(msg){
+		   message('System','Secret: '+msg);
+		 });
+		 setInterval(function() { 
+		    socket2.emit('timeLeft');
+		   },1000);
+		 })
 	});
 
 	socket.on('error', function (e) {
 	  message('System', e ? e : 'A unknown error occurred');
 	});
- });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
