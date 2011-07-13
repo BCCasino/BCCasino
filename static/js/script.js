64c381b0352a07f695011f883008b8dec71cc84f
cold <ahuntdesign@gmail.com>
2011-07-13 08:05:45 -0400

    Withdraw clientside error checking fix.

--- static/js/script.js
+++ static/js/script.js
@@ -101,7 +101,7 @@ $(function() {
 	$("#btnWithdraw").click(function() {
 		var max = parseFloat($("#balance").html());
 		var amt = parseFloat(prompt("How much do you want to withdraw? You can currently withdraw up to " + max + " BTC.", "0.00"));
-		if (amt == NaN || amt < 0 || amt > max) {
+		if (amt == NaN || amt <= 0 || amt > max) {
 			message("Error", "Invalid withdraw amount.");
 			return;
 		}
